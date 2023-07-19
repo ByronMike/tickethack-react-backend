6c40addae6aa9e3931cbdc75545e06cd06803be4
Michael <auger.michaell@gmail.com>
2023-07-19 16:49:22 +0200

    cart fonctional

--- routes/cart.js
+++ routes/cart.js
@@ -1,20 +1,21 @@
 // OBJECTIF : créer une route pour enregister dans le panier
 
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-const Trip = require('../models/trips');
-const Booking = require('../models/bookings');
-const { checkBody } = require('../modules/checkBody');
+const Trip = require("../models/trips");
+const Booking = require("../models/bookings");
+const moment = require("moment");
+const { checkBody } = require("../modules/checkBody");
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   // checkBody *** (bas de page)
-  if (!checkBody(req.body, ['tripId'])) {
-    res.json({ result: false, error: 'Missing trip ID' });
+  if (!checkBody(req.body, ["tripId"])) {
+    res.json({ result: false, error: "Missing trip ID" });
     return;
   }
 
-  Trip.findById(req.body.tripId).then(trip => {
+  Trip.findById(req.body.tripId).then((trip) => {
     if (trip) {
       const newBooking = new Booking({
         trip: trip._id,
@@ -25,32 +26,42 @@ router.post('/', (req, res) => {
         res.json({ result: true });
       });
     } else {
-      res.json({ result: false, error: 'Trip not found' });
+      res.json({ result: false, error: "Trip not found" });
     }
   });
 });
 
 // OBJECTIF : créer une route pour afficher les informations des trajets bookés
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   Booking.find({ isPaid: false })
-    .populate('trip')
-    .then(bookings => {
+    .populate("trip")
+    .then((bookings) => {
+      // console.log("backend booking", bookings);
       if (bookings.length > 0) {
-        res.json({ result: true, data: bookings });
+        const modifiedBookings = bookings.map((booking) => {
+          const modifiedBookings = { ...booking._doc }; // Create a copy of the trip object
+          
+          // Add hour and minute properties to the modified trip object
+          modifiedBookings.hour = moment(booking.date).format("HH");
+          modifiedBookings.minute = moment(booking.date).format("mm");
+      
+          return modifiedBookings;
+        });
+        res.json({ result: true, data: modifiedBookings });
       } else {
-        res.json({ result: false, error: 'No bookings found' });
+        res.json({ result: false, error: "No bookings found" });
       }
     });
 });
 
 // OBJECTIF : créer une route pour supprimer les trajets selon ID
 // Note : deletedCount is a property given by mongoose
-router.delete('/:tripId', (req, res) => {
+router.delete("/:tripId", (req, res) => {
   Booking.deleteOne({ trip: req.params.tripId }).then(({ deletedCount }) => {
     Booking.find({ isPaid: false })
-      .populate('trip')
-      .then(bookings => {
-        res.json({ result: deletedCount > 0, bookings });
+      .populate("trip")
+      .then((bookings) => {
+        res.json({ result: deletedCount > 0, data: bookings });
       });
   });
 });
--- routes/trips.js
+++ routes/trips.js
@@ -4,22 +4,42 @@ var router = express.Router();
 const moment = require("moment");
 const Trip = require("../models/trips");
 
-router.get('/:departure/:arrival/:date', (req, res) => {
-	// Déstructuration
+router.get("/:departure/:arrival/:date", async (req, res) => {
   const { departure, arrival, date } = req.params;
 
-  Trip.find({
-		// Constructeur : new RegExp avec flag 'i' (insensible à la casse) 
-    departure: new RegExp(departure, 'i'),
-    arrival: new RegExp(arrival, 'i'),
-    date: { $gte: moment(date).startOf('day'), $lte: moment(date).endOf('day') },
-  }).then(trips => {
+  try {
+    const trips = await Trip.find({
+      departure: new RegExp(departure, "i"),
+      arrival: new RegExp(arrival, "i"),
+      date: {
+        $gte: moment(date).startOf("day"),
+        $lte: moment(date).endOf("day"),
+      },
+    });
+
+    // console.log("backends trips", trips);
+
     if (trips.length > 0) {
-      res.json({ result: true, data: trips });
+      const modifiedTrips = trips.map((trip) => {
+        const modifiedTrip = { ...trip._doc }; // Create a copy of the trip object
+        
+        // Add hour and minute properties to the modified trip object
+        modifiedTrip.hour = moment(trip.date).format("HH");
+        modifiedTrip.minute = moment(trip.date).format("mm");
+    
+        return modifiedTrip;
+      });
+    
+      console.log("modifiedTrips", modifiedTrips);
+    
+      res.json({ result: true, data: modifiedTrips });
     } else {
-      res.json({ result: false, error: 'No trip found' });
+      res.json({ result: false, error: "No trip found" });
     }
-  });
+    
+  } catch (error) {
+    res.json({ result: false, error: error.message });
+  }
 });
 
 module.exports = router;
